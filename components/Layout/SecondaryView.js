6faef9f509c5249159f6fd8dc88509c60eb49474
Lam Pham <t8phho00@students.oamk.fi>
2020-03-09 11:15:03 +0200

    add delete, edit file'

--- components/Layout/SecondaryView.js
+++ components/Layout/SecondaryView.js
@@ -3,16 +3,19 @@ import { View, Text } from 'react-native'
 import Auth from '../Auth/Auth'
 import AddPost from '../Auth/screens/AddPost'
 const SecondaryView = (props) => {
+
   if(props.token === null)
   {
       console.log('hello');
       return( <Auth apiURI = {props.apiURI}
       userLogin = {props.userLogin}
-      successScreen={props.successScreen}></Auth>)
+      successScreen={props.successScreen}
+      updateData = {props.updateData}></Auth>)
   }
   else{
   return (
-    <AddPost user={props.user} token={props.token} apiURI = {props.apiURI}></AddPost>
+    <AddPost user={props.user} token={props.token} apiURI = {props.apiURI}
+    updateData = {props.updateData}></AddPost>
   )
   }
 }
--- components/Layout/ThirdView.js
+++ components/Layout/ThirdView.js
@@ -2,19 +2,51 @@ import React from 'react'
 import { View, Text } from 'react-native'
 import Auth from '../Auth/Auth'
 import ModifyPost from '../Auth/screens/ModifyPost'
+import ModifyScreen from '../Auth/screens/ModifyScreen'
+import { NavigationContainer } from '@react-navigation/native'
+import { createStackNavigator } from '@react-navigation/stack'
+
+const Stack = createStackNavigator();
 const ThirdView = (props) => {
+  let products = props.products;
+  let user = props.user;
+  let token = props.token;
+  let apiURI = props.apiURI;
+  let updateData = props.updateData;
+  //console.log('product', props.products);
   if(props.token === null)
   {
       console.log('hello');
       return( <Auth apiURI = {props.apiURI}
       userLogin = {props.userLogin}
       successScreen={props.successScreen}
-      products = {props.products}></Auth>)
+      products = {props.products}
+      updateData = {props.updateData}></Auth>)
   }
   else{
-  return (
-    <ModifyPost user={props.user} token={props.token} products = {props.products}></ModifyPost>
-  )
+    return (
+      <View style={{ flex: 1 }}>
+       <Stack.Navigator>
+          <Stack.Screen
+            name="Management"
+            options={{
+              headerShown: false,
+            }}
+          >
+            {props => <ModifyPost  {...props} updateData = {updateData} apiURI =  {apiURI} user={user} token={token} products={products}></ModifyPost>}
+          </Stack.Screen>
+          <Stack.Screen
+            name="Update"
+            options={{
+              headerShown: false,
+            }}
+          >
+            {props => <ModifyScreen  {...props} updateData = {updateData} apiURI = {apiURI} user={user} token={token} products={products}></ModifyScreen>}
+          </Stack.Screen>
+          </Stack.Navigator>
+      </View>
+
+    )
   }
 }
 
